ab67f125400a2bde0efe2ff76c4ab36f40191c13
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-20 23:15:16 +0100

    Added file for interview

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module for pascal triangle implementation
+"""
+
+def pascal_triangle(n):
+    """ Simple implementation of pacal triangle
+    """
+    res = [[1] * i for i in range(1, n + 1)]
+    for row in range(2, n):
+        for i in range(1, len(res[row]) - 1):
+            res[row][i] = res[row - 1][i - 1] + res[row - 1][i]
+    return res
