898b987768e4e0f96f1f17c84fd0379ee3320c01
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-27 17:21:30 +0100

    Fix 1 for project

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,9 +9,10 @@ def canUnlockAll(boxes):
     # Store all accessible boxes in a list
     keys = boxes[0]
     for key in keys:
-        for box in boxes[key]:
-            if box not in keys:
-                keys.append(box)
+        if key < len(boxes):
+            for box in boxes[key]:
+                if box not in keys:
+                    keys.append(box)
     # Check if all avaiable boxes are stored in keys
     for i in range(1, len(boxes)):
         if (i in keys):
