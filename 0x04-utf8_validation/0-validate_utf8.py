94713416efd14b8b820d89fbd33711aab75ff145
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-30 17:11:22 +0100

    Fix 1 for project

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,17 +8,19 @@ def validUTF8(data):
     """
     count = 0
     for x in data:
+        while x > 256:
+            x -= 256
         if count == 0:
-            if x >> 5 == 0b110: # Check if the msbs are 110
+            if x >> 5 == 0b110:  # Check if the msbs are 110
                 count = 1
-            elif x >> 4 == 0b1110: # Check if the msbs are 1110
+            elif x >> 4 == 0b1110:  # Check if the msbs are 1110
                 count = 2
-            elif x >> 3 == 0b11110: # Check if the msbs are 11110
+            elif x >> 3 == 0b11110:  # Check if the msbs are 11110
                 count = 3
-            elif x >> 7 != 0: # Check if the number starts with 1
+            elif x >> 7 != 0:  # Check if the number starts with 1
                 return False
         else:
-            if x >> 6 != 0b10: # Check if the continuation byte starts with 10
+            if x >> 6 != 0b10:  # Chek if the continuation byte starts with 10
                 return False
-            count = count - 1 # If it does, move to the next byte
-    return (count == 0) # Check if there are no missing continuation bytes
+            count = count - 1  # If it does, move to the next byte
+    return (count == 0)  # Check if there are no missing continuation bytes
