c3514a2fdbc6cb201c5ba9d34bc4f11905e681d5
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-01 20:11:06 +0100

    Testing 3

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,31 @@
 #!/usr/bin/node
-// JS file to use request module to get information from Star Wars API
+
 const request = require('request');
 
-if (process.argv.length > 2) {
-  const StarWarsUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-  request(StarWarsUrl, (error, response, body) => {
-    // Print error if error occured
-    if (error) { console.log(error); }
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
 
-    // Store array of character urls into variable
-    const json = JSON.parse(body);
-    const charactersURL = json.characters;
-    // Store an array of promises based on each character
-    const namesURL = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (err, res, bdy) => {
-          if (err) { reject(err); }
-          resolve(JSON.parse(bdy).name);
-        });
-      }));
-    // Resolve all the promises
-    Promise.all(namesURL).then(names => console.log(names.join('\n')))
-      .catch(e => console.log(e));
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
   });
 }
+
+request(movieEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
+  }
+});
