312aadf2a136ccaf604c59c4c9c23f8de43785a4
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-01 20:03:58 +0100

    Fix 2 for project

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,27 @@
 // JS file to use request module to get information from Star Wars API
 const request = require('request');
 
-// if (process.argv.length <= 2) { exit(0); }
+if (process.argv.length > 2) {
 
-const StarWarsUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+  const StarWarsUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request(StarWarsUrl, (error, response, body) => {
-  // Print error if error occured
-  if (error) { console.log(error); }
+  request(StarWarsUrl, (error, response, body) => {
+    // Print error if error occured
+    if (error) { console.log(error); }
 
-  // Store array of character urls into variable
-  const json = JSON.parse(body);
-  const charactersURL = json.characters;
-  // console.log(characters);
-  const namesURL = charactersURL.map(
-    url => new Promise((resolve, reject) => {
-      request(url, (err, res, bdy) => {
-        if (err) { reject(err); }
-        resolve(JSON.parse(bdy).name);
-      });
-    }));
-  // Resolve all the promises
-  Promise.all(namesURL).then(names => console.log(names.join('\n')))
-    .catch(e => console.log(e));
-});
+    // Store array of character urls into variable
+    const json = JSON.parse(body);
+    const charactersURL = json.characters;
+    // Store an array of promises based on each character
+    const namesURL = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (err, res, bdy) => {
+          if (err) { reject(err); }
+          resolve(JSON.parse(bdy).name);
+        });
+      }));
+    // Resolve all the promises
+    Promise.all(namesURL).then(names => console.log(names.join('\n')))
+      .catch(e => console.log(e));
+  });
+}
