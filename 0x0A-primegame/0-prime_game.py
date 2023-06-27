f2a1751d333a93af499962a492cb6fe265e41503
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-27 09:32:02 -0700

    Added folder for prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module to implement function for prime game
+"""
+
+
+def isWinner(x, nums):
+    """ Predict the winner between Maria and Ben
+    Return:
+      - Name of the player that wins
+    """
